4839d3ff060ce3723343d04bf11fb52b01477109
alegonzalezM <68816355+alegonzalezM@users.noreply.github.com>
2022-10-25 21:07:25 -0300

    Add files via upload

---
+++ Desktop/Cursos/Codo-a-codo-Agosto 2022/Bikes/js/elementos.js
@@ -0,0 +1,118 @@
+var cabecera=`
+
+<div class="grid-row">
+<div class="div-row">
+</div>
+<div id="div-row" ><img src="imagenes/bike-shop-logo.png" id="logo" alt="logo">
+</div>
+<div class="div-row" >
+<div class="contenedor-redes">
+	    <a href="https://www.facebook.com"><i class="fa fa-brands fa-facebook  fa-2x"></i></a>
+		<a href="https://https://www.instagram.com"><i class="fa fa-brands fa-instagram fa-2x"></i></a>
+	    <a href="#"><i class="fa fa-whatsapp fa-2x" ></i></a>
+		<a href="#"><i class=" fa fa-brands fa-twitter fa-2x"></i></a>
+		<a href="#"><i class="fa fa-map-marker fa-2x"></i></a>
+	    <a href="mailto:bikeshop@gmail.com"><i class="fa fa-envelope fa-2x"></i></a>
+</div></div></div> ` ;
+
+document.getElementById("cabecera").innerHTML= cabecera;
+
+//----------------------------------------------------
+
+var nav=
+`<div id="header">
+			<ul class="nav">
+				<li><a href="index.html">INICIO</a></li>
+				<li><a href="#">BICICLETAS</a>
+					<ul class="lista-menu">
+						<li><a href="bicicletas-MTB.html">MTB</a></li>
+						<li><a href="#">Urbanas</a></li>
+						<li><a href="#">Infantiles</a></li>
+						<li><a href="#">BMX</a></li>
+					</ul></li>
+				<li><a href="#">ACCESORIOS</a>
+					<ul class="lista-menu">		
+						<li><a href="cascos.html">Cascos</a></li>
+						<li><a href="#">Luces</a></li>
+						<li><a href="#">Indumentaria</a></li>
+					</ul></li>
+				<li><a href="#">EVENTOS</a>
+					<ul class="lista-menu">
+						<li><a href="#">Siete Lagos</a></li>
+						<li><a href="#">Altas Cumbres</a></li>
+						<li><a href="formulario.html">Inscripción</a></li>
+						
+					</ul></li>
+				<li><a href="contacto.html">CONTACTO</a></li>
+			</ul>
+		</div> `
+
+document.getElementById("row_exp").innerHTML=nav ;
+
+//----------------------------------------------------
+
+
+//----------------------------------------------------
+
+var miFooter= `<div class="contenedor_footer"><p></p><p>Av Cordoba 3333 - CABA <br> 155548969</p>
+	<p>Copyright 2022 by Ramsnes Data. All Rights Reserved.</p>
+	
+	<div class="contenedor-redes">
+	    <a href="https://www.facebook.com"><i class="fa fa-brands fa-facebook  fa-2x"></i></a>
+		<a href="https://https://www.instagram.com"><i class="fa fa-brands fa-instagram fa-2x"></i></a>
+	    <a href="#"><i class="fa fa-whatsapp fa-2x" ></i></a>
+		<a href="#"><i class=" fa fa-brands fa-twitter fa-2x"></i></a>
+		<a href="#"><i class="fa fa-map-marker fa-2x"></i></a>
+	    <a href="mailto:bikeshop@gmail.com"><i class="fa fa-envelope fa-2x"></i></a>
+</div></div>
+`;
+
+	
+
+document.getElementById("footer").innerHTML=miFooter;
+
+
+//-----------------------------------------------------------------------
+ const codVue= Vue.createApp({      
+  data(){
+	  return{
+    cascos:[
+      {codigo:"1",
+      imagen:"imagenes/casco1.jpg",
+      nombre:"Casco Bell",
+      descripcion:"Texto descriptivo"
+      },
+      {codigo:"2",
+      imagen:"imagenes/casco2.jpg",
+      nombre:"Cacso MTB",
+      descripcion:"Texto descriptivo"
+      }, 
+	  {codigo:"1",
+      imagen:"imagenes/casco1.jpg",
+      nombre:"Casco Bell",
+      descripcion:"Texto descriptivo"
+      },
+      {codigo:"2",
+      imagen:"imagenes/casco2.jpg",
+      nombre:"Cacso MTB",
+      descripcion:"Texto descriptivo"
+      }, 
+	 {codigo:"2",
+      imagen:"imagenes/casco2.jpg",
+      nombre:"Cacso MTB",
+      descripcion:"Texto descriptivo"
+      }, 
+	  {codigo:"1",
+      imagen:"imagenes/casco1.jpg",
+      nombre:"Casco Bell",
+      descripcion:"Texto descriptivo"
+      },
+        
+    ],
+    
+ 
+  }}
+}).mount("#divVue");
+//-------------------------------------------------------------
+
+
